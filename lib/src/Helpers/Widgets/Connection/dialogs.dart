44fc4177198b97616e1b79df68776da6f588b6f1
alzoldik <51909435+alzoldik@users.noreply.github.com>
2021-04-14 14:53:12 +0200

    filtering

--- lib/src/Helpers/Widgets/Connection/dialogs.dart
+++ lib/src/Helpers/Widgets/Connection/dialogs.dart
@@ -5,25 +5,26 @@ import 'package:flutter/material.dart';
 Future<dynamic> showDialogNotInternet(BuildContext context) {
   return showDialog(
       context: context,
-      child: CupertinoAlertDialog(
-        title: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text("  فشل الاتصال بالانترنت    "),
-              Icon(
-                Icons.warning,
+      builder: (c) => CupertinoAlertDialog(
+            title: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("  فشل الاتصال بالانترنت    "),
+                  Icon(
+                    Icons.warning,
+                  ),
+                ],
               ),
+            ),
+            content: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Text("! من فضلك اعد الاتصال بالانترنت"),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                  onPressed: AppSettings.openWIFISettings,
+                  child: Text("الاعدادات"))
             ],
-          ),
-        ),
-        content: Padding(
-          padding: const EdgeInsets.all(5.0),
-          child: Text("! من فضلك اعد الاتصال بالانترنت"),
-        ),
-        actions: <Widget>[
-          FlatButton(
-              onPressed: AppSettings.openWIFISettings, child: Text("الاعدادات"))
-        ],
-      ));
+          ));
 }
